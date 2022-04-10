b4e567b274a691c8cb33e3aefe6308d2be08e457
saadhanaganesh13 <saadhana.ganesh13@gmail.com>
2022-04-10 15:42:21 +0530

    3:41

--- forms/contact.php
+++ forms/contact.php
@@ -4,7 +4,7 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "emailaddress@here.com";
+$recipient = "saadhana.ganesh13@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
