38d37725f4519cc5e3cba3cd04bb4ed2f692fbff
saadhanaganesh13 <saadhana.ganesh13@gmail.com>
2022-04-10 20:21:56 +0530

    8.21

--- forms/contact.php
+++ forms/contact.php
@@ -10,4 +10,17 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 
+
+/* server { 
+  listen 80;
+  listen 443 ssl;    
+  server_name airbrake.io;    
+  location /users/create {
+      if ($request_method = POST) {
+          return 405 https://airbrake.io/users/create$request_uri;
+      }
+  }
+}
+ */
+
 ?>
