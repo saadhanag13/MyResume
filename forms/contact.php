77cd0fdee41d1740c0d0b75dd49a136fe996c22e
saadhanaganesh13 <saadhana.ganesh13@gmail.com>
2022-06-25 14:34:55 +0530

    2.32

--- forms/contact.php
+++ forms/contact.php
@@ -1,16 +1,34 @@
 <?php
+    $message_sent = false; 
+    
+    if(isset($_POST['email']) && $_POST['email'] != ''){
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+            
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $subject = $_POST['subject'];
+            $message = $_POST['message'];
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-$formcontent=" From: $name \n Message: $message";
-$recipient = "saadhana.ganesh@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+            $to = "saadhana.ganesh13@gmail.com";
+            $body= "";
+
+            $body .= "From: " .$name. "\r\n";
+            $body .= "Email: " .$email. "\r\n";
+            $body .= "Message: " .$message. "\r\n";
+
+            //mail($to,$subject,$body);
+            $message_sent = true; 
+
+        }
+        
+    }
+
+    
+
+    //$formcontent=" From: $name \n Message: $message";
+    //$recipient = "saadhana.ganesh@gmail.com";
+    //$mailheader = "From: $email \r\n";
+    //mail($recipient, $formcontent, $mailheader) or die("Error!");
+    //echo "Thank You!";
 ?>
