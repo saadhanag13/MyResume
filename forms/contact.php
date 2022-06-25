1b2d953a6895f92741eb1b17c2b4cc52eb5cd113
saadhanaganesh13 <saadhana.ganesh13@gmail.com>
2022-06-25 14:41:03 +0530

    2.41

--- forms/contact.php
+++ forms/contact.php
@@ -3,9 +3,9 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Message: $message";
-$recipient = "saadhana.ganesh@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $formcontent, $mailheader) or die("Error!");
+$to =" From: $name \n Message: $message";
+$recipient = "saadhana.ganesh13@gmail.com";
+$from = "From: $email \r\n";
+mail($recipient, $to, $from) or die("Error!");
 echo "Thank You!";
 ?>
